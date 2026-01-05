b78254f371d651ab86ad153f3dc9f69f637d399f
Shruti627 <shrutisangvikar627@gmail.com>
2026-01-05 15:18:03 +0530

    added to app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,10 @@
 // src/app.js
 import express from "express";
-
+import feedRoutes from "./routes/feed.routes.js";
 const app = express();
 
 // Global middlewares
+app.use("/api/feed", feedRoutes);
 app.use(express.json());
 
 export default app;
